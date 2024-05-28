316ab9f3d42ad8dfb680f1e55ba79883b2174383
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2024-05-28 13:02:50 -0600

    Adding barrel file

---
+++ lib/presentation/widgets/widgets.dart
@@ -0,0 +1,4 @@
+
+export 'package:payment_gateway/presentation/widgets/shared/reusable_box_decoration.dart';
+
+export 'package:payment_gateway/presentation/widgets/inputs/reusable_text_form_field.dart';
