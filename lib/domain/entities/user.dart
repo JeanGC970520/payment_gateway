fc2a986b7cc032ed2fd9356cda63555f26f06fd4
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2024-05-28 17:24:59 -0600

    Adding User entity

---
+++ lib/domain/entities/user.dart
@@ -0,0 +1,21 @@
+
+
+class User {
+
+  final String name;
+  final String address;
+  final String city;
+  final String stateCode;
+  final String countryCode;
+  final String zipcode;
+
+  User({
+    required this.name, 
+    required this.address, 
+    required this.city, 
+    required this.stateCode, 
+    required this.countryCode, 
+    required this.zipcode,
+  });
+
+}
