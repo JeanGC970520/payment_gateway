9ea331a2d2442e35aa8448eb870f0e04b24dcdc7
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2024-05-30 11:49:08 -0600

    Adding more properties to the paymenten intent

--- lib/presentation/blocs/payment_gateway/payment_gateway_bloc.dart
+++ lib/presentation/blocs/payment_gateway/payment_gateway_bloc.dart
@@ -5,6 +5,7 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:payment_gateway/logger.dart';
 
 import '../../../domain/entities/user.dart';
 
@@ -21,12 +22,24 @@ class PaymentGatewayBloc extends Bloc<PaymentGatewayEvent, PaymentGatewayState> 
     on<MakePaymentEvent>(_mapMakePaymentEventToState);
   }
 
-  Future<Map<dynamic, dynamic>?> createPaymentIntent(String amount, String currency) async { 
+  Future<Map<dynamic, dynamic>?> createPaymentIntent({
+    required String amount, 
+    required String currency,
+    required User userInfo,
+    required String description,
+  }) async { 
     try { 
       Map<String, dynamic> body = { 
         'amount': ((int.parse(amount)) * 100).toString(), 
         'currency': currency, 
         'payment_method_types[]': 'card', 
+        'description' : description,
+        'shipping[name]' : userInfo.name,
+        'shipping[address][line1]' : userInfo.address,
+        'shipping[address][postal_code]' : userInfo.zipcode,
+        'shipping[address][city]' : userInfo.city,
+        'shipping[address][state]' : userInfo.stateCode,
+        'shipping[address][country]' : userInfo.countryCode,
       }; 
 
       dioStripe.options.headers = { 
@@ -38,13 +51,13 @@ class PaymentGatewayBloc extends Bloc<PaymentGatewayEvent, PaymentGatewayState> 
         data: body, 
       ); 
 
-      // debugPrint('Payment Intent Body: ${response.data.toString()}');
+      // logger.d('Payment Intent Body: ${response.data.toString()}');
       return response.data; 
     } catch (err) { 
-      debugPrint('Error charging user: ${err.toString()}'); 
+      logger.e('Error charging user: ${err.toString()}'); 
       return null;
     } 
-  } 
+  }
 
   Future<void> initPaymentIntent(Map<dynamic, dynamic> data) async {
     debugPrint('Initializating payment sheet');
@@ -56,12 +69,33 @@ class PaymentGatewayBloc extends Bloc<PaymentGatewayEvent, PaymentGatewayState> 
         merchantDisplayName: 'Flutter Stripe Store demo',
         paymentIntentClientSecret: data['client_secret'],
         // Customer keys
-        customerEphemeralKeySecret: data['ephemeralKey'],
+        // customerEphemeralKeySecret: data['ephemeralKey'],
         customerId: data['id'],
         style: ThemeMode.dark,
+        // appearance: const PaymentSheetAppearance(
+        //   colors: PaymentSheetAppearanceColors(
+        //     background: Colors.lightBlue,
+        //     primary: Colors.blue,
+        //     componentBorder: Colors.red,
+        //   ),
+        //   shapes: PaymentSheetShape(
+        //     borderWidth: 4,
+        //     shadow: PaymentSheetShadowParams(color: Colors.red),
+        //   ),
+        //   primaryButton: PaymentSheetPrimaryButtonAppearance(
+        //     shapes: PaymentSheetPrimaryButtonShape(blurRadius: 8),
+        //     colors: PaymentSheetPrimaryButtonTheme(
+        //       light: PaymentSheetPrimaryButtonThemeColors(
+        //         background: Color.fromARGB(255, 231, 235, 30),
+        //         text: Color.fromARGB(255, 235, 92, 30),
+        //         border: Color.fromARGB(255, 235, 92, 30),
+        //       ),
+        //     ),
+        //   ),
+        // ), 
       )
     );
-    debugPrint("Payment sheet it's ready");
+    logger.i("Payment sheet it's ready");
   }
 
   Future<bool> presentPaymentSheet() async {
@@ -69,7 +103,7 @@ class PaymentGatewayBloc extends Bloc<PaymentGatewayEvent, PaymentGatewayState> 
       await Stripe.instance.presentPaymentSheet();
       return true;
     } on StripeException catch (err) {
-      debugPrint('Error on present payment sheet: $err');
+      logger.e('Error on present payment sheet: $err');
       return false;
     }
   }
@@ -77,8 +111,13 @@ class PaymentGatewayBloc extends Bloc<PaymentGatewayEvent, PaymentGatewayState> 
   void _mapMakePaymentEventToState(
     MakePaymentEvent event, Emitter<PaymentGatewayState> emit
   ) async {
-    final response = await createPaymentIntent(event.amount, event.currency);
-    debugPrint(response?.values.toString());
+    final response = await createPaymentIntent(
+      amount: event.amount, 
+      currency: event.currency,
+      userInfo: event.userInfo,
+      description: event.description,
+    );
+    logger.d(response?.values.toString());
 
     // If create paymenent intent is not success.
     if(response == null) return;
@@ -86,7 +125,7 @@ class PaymentGatewayBloc extends Bloc<PaymentGatewayEvent, PaymentGatewayState> 
     await initPaymentIntent(response);
 
     final paymentStatus = await presentPaymentSheet();
-    debugPrint('Payment status: $paymentStatus');
+    logger.i('Payment status: $paymentStatus');
   }
 
 }
